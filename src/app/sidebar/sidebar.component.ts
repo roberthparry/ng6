356872d9b8bcd71c0c8ee99b1d9738fb35fa0a6f
roberthparry <robert.h.parry@gmail.com>
2018-10-07 00:13:16 +0100

    fix sidebar activation issues

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,7 +11,11 @@ export class SidebarComponent implements OnInit {
   currentUrl: string;
 
   constructor(private router: Router) {
-    router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
+    router.events.subscribe(route => {
+      if (route instanceof NavigationEnd) {
+        this.currentUrl = route.url;
+      }
+    });
   }
 
   ngOnInit() {
